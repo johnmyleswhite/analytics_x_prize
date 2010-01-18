37edf4344353c3ab214229f8816cf50318226ae3
John Myles White <jmw@johnmyleswhite.com>
2010-01-18 17:16:03 -0500

    Used weights from Drew's spdep code for naive KNN predictions.

--- models/naive_model_7.R
+++ models/naive_model_7.R
@@ -1,17 +1,36 @@
-# First, we define a distance
+# First, we define a distance metric.
+library(spdep)
 
-distances <- as.data.frame(expand.grid(zipcodes, zipcodes))
-names(distances) <- c('zipcode.a', 'zipcode.b')
-distances$distance <- rep(NA, nrow(distances))
+# Load shapefile of Philadelphia zips
+prj<-CRS("+proj=utm +zone=30 +units=km")
+shape.data<-readShapeSpatial("shape_files/phila_zipcodes_shp/zipcodes.shp", ID="ZIPCODE", proj4string = prj)
 
-for (i in 1:nrow(distances))
-{
-  distances[i,'distance'] <- abs(as.numeric(as.character(distances[i,1])) - as.numeric(as.character(distances[i,2])))
-}
+phi.nb<-tri2nb(coordinates(shape.data),row.names=row.names(shape.data)) # Create neighbor object
+phi.lw<-nb2listw(phi.nb)    # Create list of weights
+
+#distances <- as.data.frame(expand.grid(zipcodes, zipcodes))
+#names(distances) <- c('zipcode.a', 'zipcode.b')
+#distances$distance <- rep(NA, nrow(distances))
+
+#for (i in 1:nrow(distances))
+#{
+#  distances[i,'distance'] <- abs(as.numeric(as.character(distances[i,1])) - as.numeric(as.character(distances[i,2])))
+#}
 
 distance.metric <- function(z1, z2)
 {
-  return(with(subset(distances, zipcode.a == z1 & zipcode.b == z2), distance))
+  z1.index <- which(zipcodes == z1)
+  z2.index <- which(zipcodes == z2)
+  w.index <- which(phi.lw$neighbours[[z1.index]] == z2.index)
+  if (length(w.index) > 0)
+  {
+    distance <- phi.lw$weights[[z1.index]][w.index]
+  }
+  else
+  {
+    distance <- 0
+  }
+  return(distance)
 }
 
 for (i in 1:nrow(predicted.homicides))
@@ -24,7 +43,8 @@ for (i in 1:nrow(predicted.homicides))
   for (z in zipcodes)
   {
     p <- with(subset(homicides, Zipcode == z & Year == target[['target']] - 1), Probability)
-    weight <- 1 / (1 + distance.metric(zipcode, as.numeric(as.character(z)))^a)
+    weight <- distance.metric(zipcode, as.numeric(as.character(z)))
+    #weight <- 1 / (1 + distance.metric(zipcode, as.numeric(as.character(z)))^a)
     weights <- c(weights, weight)
     weighted.sum <- weighted.sum + weight * p
   }
