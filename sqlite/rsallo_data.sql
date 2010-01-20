1feaf0596fda88091fc1e684f3ed0c3ae6cd327f
John Myles White <jmw@johnmyleswhite.com>
2010-01-19 19:55:19 -0500

    Added rsallo data to the database; moved old CSV file into old_data.

---
+++ sqlite/rsallo_data.sql
@@ -0,0 +1,11 @@
+CREATE TABLE rsallo_data (long DOUBLE,
+                          lat DOUBLE,
+                          date DATE,
+                          age INTEGER,
+                          race VARCHAR(1),
+                          sex VARCHAR(1),
+                          time VARCHAR(4),
+                          cause VARCHAR(16),
+                          weapon VARCHAR(16),
+                          motive VARCHAR(16),
+                          zipcode VARCHAR(16));
